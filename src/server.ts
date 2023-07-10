f11ab8fd65bfebd4f0969df011bfb64e552bac39
Guilherme Marques <gmoraesm12@gmail.com>
2023-07-10 08:54:19 -0300

    chore: Change server host for render

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
-    host: env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1',
+    host: 'RENDER' in process.env ? '0.0.0.0' : '127.0.0.1',
   })
   .then(() => {
     console.log('HTTP Server Running')
